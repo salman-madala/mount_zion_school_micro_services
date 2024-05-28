e70fc48e3c8616fa1dab50dbb7a0dd69df1c203e
smadala <smadala@altair.com>
2024-05-28 15:08:28 +0530

    hide the form based authentication added in memory basic authentication using UserDetailsService which is inside the springframework-security-core Application

--- gateway/src/main/java/com/emabarkx/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/emabarkx/gateway/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.emabarkx.gateway.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,8 +20,19 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        UserDetails user = User.withUsername("user").password("{noop}user").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+
 }
