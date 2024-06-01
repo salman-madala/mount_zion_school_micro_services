b87081e79e88fde7250ab976c56cfc4f37bce0e6
smadala <smadala@altair.com>
2024-06-01 17:21:51 +0530

    Added H2 databsed for spring security log in with username and password using the httpBasic authentication

--- gateway/src/main/java/com/emabarkx/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/emabarkx/gateway/config/SecurityConfig.java
@@ -1,38 +1,71 @@
 package com.emabarkx.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    DataSource dataSource;
+
+    public SecurityConfig(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 
 
     @Bean
     public UserDetailsService userDetailsService(){
+
+        /* -----------in memory database example --------------------*/
+//        UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+//        UserDetails user = User.withUsername("user").password("{noop}user").roles("USER").build();
+//          return new InMemoryUserDetailsManager(admin,user);
+
+
+        /* -----------using h2 database--------*/
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
         UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
         UserDetails user = User.withUsername("user").password("{noop}user").roles("USER").build();
+        jdbcUserDetailsManager.createUser(admin);
+        jdbcUserDetailsManager.createUser(user);
 
-        return new InMemoryUserDetailsManager(admin,user);
+        return jdbcUserDetailsManager;
+
+
+
     }
 
 }
